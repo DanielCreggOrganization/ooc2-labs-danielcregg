f89fa7399c821372ed3c81838cf20d2f78007801
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-25 14:28:23 +0000

    Add array

---
+++ ie/atu/refresh/ArrayExample.java
@@ -0,0 +1,12 @@
+package ie.atu.refresh;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Create Array
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        // String[] daysOfWeek = new String[7];
+        // daysOfWeek[0] = "Monday";
+
+        System.out.println(daysOfWeek[4]);
+    }
+}
