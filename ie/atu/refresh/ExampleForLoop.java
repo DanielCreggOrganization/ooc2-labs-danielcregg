91aa053ed2d1a1cdc55ebb60c85185f3646e1d51
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-25 14:10:34 +0000

    Added example for loop

---
+++ ie/atu/refresh/ExampleForLoop.java
@@ -0,0 +1,9 @@
+package ie.atu.refresh;
+
+public class ExampleForLoop {
+    public static void main(String[] args) {
+        for (int x = 10; x >= 1; x--) {
+            System.out.println("The value of x is : " + x);
+        }
+    }
+}
