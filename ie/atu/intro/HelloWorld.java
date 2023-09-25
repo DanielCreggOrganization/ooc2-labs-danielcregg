c096cab04dbe46e0b6314c82f592ba5fec05620c
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-25 13:50:22 +0000

    First Lab start

--- ie/atu/intro/HelloWorld.java
+++ ie/atu/intro/HelloWorld.java
@@ -4,6 +4,6 @@ package ie.atu.intro;
 public class HelloWorld {
     public static void main(String[] args) {
         // Prints 'Hello, World!' to the console.
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Daniel!");
     }
 }
--- ie/atu/intro/HelloWorldFX.java
+++ ie/atu/intro/HelloWorldFX.java
@@ -14,7 +14,7 @@ public class HelloWorldFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'Hello world'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
