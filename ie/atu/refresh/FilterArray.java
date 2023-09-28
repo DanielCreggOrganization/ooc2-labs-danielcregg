8180fe3ddb747f345377f141908ef23dda856b5d
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-28 13:52:38 +0000

    Add integer array holding 1 to 10

--- ie/atu/refresh/FilterArray.java
+++ ie/atu/refresh/FilterArray.java
@@ -2,6 +2,7 @@ package ie.atu.refresh;
 
 public class FilterArray {
     public static void main(String[] args) {
-        
+        // Create integer array which holds number 1 to 10
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     }
 }
