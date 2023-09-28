73089a82fb349200fae4b23515bb3fa3c82b17d2
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-28 13:54:20 +0000

    use for loop to print out the array

--- ie/atu/refresh/FilterArray.java
+++ ie/atu/refresh/FilterArray.java
@@ -3,6 +3,11 @@ package ie.atu.refresh;
 public class FilterArray {
     public static void main(String[] args) {
         // Create integer array which holds number 1 to 10
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Use a for loop to iterate through the array and print out each element
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
