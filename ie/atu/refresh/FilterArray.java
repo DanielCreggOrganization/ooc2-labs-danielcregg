b800a0a762eb6427585c3affec51271ffb5a61bb
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-28 14:02:34 +0000

    Sum up all even numbers

--- ie/atu/refresh/FilterArray.java
+++ ie/atu/refresh/FilterArray.java
@@ -2,6 +2,8 @@ package ie.atu.refresh;
 
 public class FilterArray {
     public static void main(String[] args) {
+        // Create integer variable to store the sum of all even numbers in the array
+        int sum = 0;
 
         // Create integer array which holds number 1 to 100
         int[] numbers = new int[100];
@@ -10,14 +12,18 @@ public class FilterArray {
         // 100
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
-        }
+        } // End of for loop
 
         // Use a for loop to iterate through the array and print out each element
         for (int i = 0; i < numbers.length; i++) {
-            // Print all even numbers in the array
+            // Print all even numbers in the array. Find the sum of all even numbers in the
+            // array and print that too.
             if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
                 System.out.println(numbers[i]);
-            }
-        }
-    }
-}
+            } // End of if statement
+        } // End of for loop
+        // Print the sum of all even numbers in the array
+        System.out.println("The sum of all even numbers in the array is: " + sum);
+    } // End of main
+} // End of class
