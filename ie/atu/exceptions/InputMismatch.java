2b5d1b963d8c8e85f1bdcd0dbb9497fb5f818ea8
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-16 14:22:19 +0000

    updates

---
+++ ie/atu/exceptions/InputMismatch.java
@@ -0,0 +1,35 @@
+package ie.atu.exceptions;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class InputMismatch {
+
+	public static void main(String args[]) {
+
+		// Create integer variable to store user age
+		int age = 0;
+
+		// Ask user what age they are
+		System.out.println("Please enter your age: ");
+
+		// Create scanner object to capture user input
+		Scanner userInput = new Scanner(System.in);
+
+		// Try to read in the users age
+		try {
+			age = userInput.nextInt();
+			System.out.println("You are " + age + " years old.");
+		} catch (InputMismatchException inputMisEx) {
+			System.out.print(inputMisEx);
+			System.err.println("\nYou did not enter an integer!\n");
+			// System.err.println("\nException in thread: " + inputMisEx + "\n\tat " +
+			// inputMisEx.getStackTrace()[inputMisEx.getStackTrace().length - 1]);
+        } finally { // finally executes whether or not an exception is thrown
+			userInput.close(); // Close Scanner object
+		}
+
+		System.out.println("The program has ended. Goodbye!");
+
+	} // End main
+} // End class
